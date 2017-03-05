fcaf19b398c14e5365839b0a3817f48f081bd65e
Ales Katona <almindor@gmail.com>
2017-03-04 17:19:05 -0700

    * add master translation file (lowercased)

---
+++ translations/harbour-jtox.ts
@@ -0,0 +1,461 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="41"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="52"/>
+        <source>jTox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="52"/>
+        <source>by Aleš Katona.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="63"/>
+        <source>Toxcore version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="75"/>
+        <source>jTox is free software licensed under the GPL v3 available at &lt;a href=&apos;https://github.com/almindor/harbour-jTox&apos;&gt;github&lt;/a&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="90"/>
+        <source>The Tox protocol is EXPERIMENTAL! The protocol has not been audited for security and can contain vulnerabilities. Use at your own risk.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Account</name>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="37"/>
+        <source>Importing Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="57"/>
+        <source>Import Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="62"/>
+        <source>Export Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="63"/>
+        <source>Exporting Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="81"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="85"/>
+        <source>Show QR Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="98"/>
+        <source>ToxID copied to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="129"/>
+        <source>Tox ID: touch to copy to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="141"/>
+        <location filename="../qml/pages/Account.qml" line="143"/>
+        <source>Nospam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="154"/>
+        <location filename="../qml/pages/Account.qml" line="156"/>
+        <source>User name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="167"/>
+        <source>Status message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="168"/>
+        <source>Hi from jTox on Sailfish</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="173"/>
+        <source>Chat history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="178"/>
+        <source>Wipe history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="179"/>
+        <source>Wiping chat history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="195"/>
+        <source>password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="201"/>
+        <source>Private</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="218"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="218"/>
+        <source>Register</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Account.qml" line="230"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AddFriend</name>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="46"/>
+        <source>Add Friend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="57"/>
+        <source>or</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="58"/>
+        <source>Address or handle@</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="96"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="97"/>
+        <source>Please add me to friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="109"/>
+        <source>Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="110"/>
+        <source>Address not resolved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="120"/>
+        <source>Resolved Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddFriend.qml" line="132"/>
+        <source>Resolve Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="88"/>
+        <source>Messages: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FriendItem</name>
+    <message>
+        <location filename="../qml/components/FriendItem.qml" line="23"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/FriendItem.qml" line="24"/>
+        <source>Removing</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Friends</name>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="43"/>
+        <source>Friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="70"/>
+        <source>Pending friend requests: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="79"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="84"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="89"/>
+        <source>Add Friend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="97"/>
+        <source>Ready</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="109"/>
+        <source>Away</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Friends.qml" line="121"/>
+        <source>Busy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Import</name>
+    <message>
+        <location filename="../qml/pages/Import.qml" line="29"/>
+        <source>Import Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Import.qml" line="43"/>
+        <source>WARNING!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Import.qml" line="44"/>
+        <source>Importing an account will delete existing account including contacts and messages!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Import.qml" line="53"/>
+        <source>Documents Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Import.qml" line="64"/>
+        <source>No account files found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Import.qml" line="65"/>
+        <source>No account *.tox files found in Documents folder. Copy account file to Documents and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InputItem</name>
+    <message>
+        <location filename="../qml/components/InputItem.qml" line="24"/>
+        <source>is typing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/InputItem.qml" line="58"/>
+        <source>Type your message here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/InputItem.qml" line="58"/>
+        <source>is offline</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JTOX::ToxCore</name>
+    <message>
+        <location filename="../src/toxcore.cpp" line="512"/>
+        <source>Offline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/toxcore.cpp" line="513"/>
+        <source>Ready</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/toxcore.cpp" line="514"/>
+        <source>Away</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/toxcore.cpp" line="515"/>
+        <source>Busy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/toxcore.cpp" line="518"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JTOX::ToxInitializer</name>
+    <message>
+        <location filename="../src/toxcore.cpp" line="141"/>
+        <source>Hi from jTox on Sailfish</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MessageItem</name>
+    <message>
+        <location filename="../qml/components/MessageItem.qml" line="17"/>
+        <source>Copy to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/MessageItem.qml" line="20"/>
+        <source>Message copied to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Password</name>
+    <message>
+        <location filename="../qml/pages/Password.qml" line="47"/>
+        <source>New Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Password.qml" line="48"/>
+        <source>Creating Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Password.qml" line="63"/>
+        <source>Password too short</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Password.qml" line="64"/>
+        <source>New account password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Password.qml" line="64"/>
+        <source>Account password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Password.qml" line="84"/>
+        <source>Invalid password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RequestItem</name>
+    <message>
+        <location filename="../qml/components/RequestItem.qml" line="21"/>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RequestItem.qml" line="22"/>
+        <source>Accepting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RequestItem.qml" line="36"/>
+        <source>Reject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RequestItem.qml" line="37"/>
+        <source>Rejecting</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Requests</name>
+    <message>
+        <location filename="../qml/pages/Requests.qml" line="32"/>
+        <source>Friend Requests</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>harbour-jtox</name>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="48"/>
+        <source>New friend request</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="49"/>
+        <source>Account Imported</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="50"/>
+        <source>Account Exported: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="51"/>
+        <source>New account created</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="52"/>
+        <source>Chat history wiped</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="63"/>
+        <source>Message from</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="70"/>
+        <source>Registered on </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-jtox.qml" line="71"/>
+        <source>Removed from </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
