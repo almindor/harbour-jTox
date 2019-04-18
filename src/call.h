a2c2cc4bb432be2f17ea9635b8c7b90e04bd58aa
Ales Katona <almindor@gmail.com>
2019-04-17 18:30:47 -0600

    evert "abstract into calls"
    This reverts commit ab2ec4ad3b54bd70fff9b2414ddcedfd65839f74.

--- src/call.h
+++ src/call.h
@@ -1,41 +1,12 @@
 #ifndef CALL_H
 #define CALL_H
 
-#include <QThread>
-#include <QAudioInput>
-#include <QAudioOutput>
-#include "tox/toxav.h"
-
 namespace JTOX {
 
-    class Call : public QThread
+    class Call
     {
     public:
-        Call(ToxAV* toxAV, quint32 friend_id);
-
-        // starts audio input + input handling thread
-        bool init();
-        // stops input/output and thread in a controlled manner
-        void finish();
-        // sets up audio output if not done yet and pushes frame data
-        bool handleIncomingFrame(const qint16* pcm, size_t sample_count, quint8 channels, quint32 sampling_rate);
-
-        void run() override; // handles audio input -> tox
-    private:
-        ToxAV* fToxAV;
-        quint32 fFriendID;
-        QAudioInput* fAudioInput;
-        QAudioOutput* fAudioOutput;
-        QIODevice* fAudioInputPipe;
-        QIODevice* fAudioOutputPipe;
-        bool fQuit;
-
-        bool sendNextAudioFrame();
-        bool startAudioInput();
-        void stopAudioInput();
-        bool startAudioOutput(int channels, int samplingRate);
-        void stopAudioOutput();
-        const QAudioFormat defaultAudioFormat(int channels, int samplingRate) const;
+        Call();
     };
 
 }
